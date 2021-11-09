e1c50b0c6c1a03ab5dfda2d3a900c7b917cd86cc
shubham8528 <shubham.bisht@etelligens.in>
2021-11-09 18:32:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
+import { Route, Switch } from 'react-router';
+import Services from './Services';
 import './App.css';
+import Contact from './Contect';
+import Home from './Home';
+
+import Costumerdetails from './Costumerdetails';
+import Login from './Login';
+// import { useState } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Switch>
+        <Route exact path='/' component={Login} />
+        <Route path='/home' component={Home} />
+        <Route path='/services' component={Services} />
+        <Route path='/contact' component={Contact} />
+        <Route path='/constumerdetails' component={Costumerdetails} />
+      </Switch>
+    </>
   );
 }
 
